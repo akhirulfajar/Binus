81663f24f6711d7297e35a35169e0c9ed3b467a7
akhirulfajar <akhirulfajar@gmail.com>
2024-03-19 12:20:48 +0700

    Learning from start, variables, data types, and arrays

---
+++ Task/Personal/Semester 1/02. Algoritma dan Pemrograman/Learning/1-helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// stdio.h adalah directory yang harus dipanggil, karena kita memanfaatkan salah satu fungsi nya, yaitu fungsi "printf"
+// Informasi lengkap soal directory ada di https://www.tutorialspoint.com/c_standard_library/index.htm
+
+int main() {
+    printf("Hello World! I'm testing my first C Program!");
+    return 0;
+}
+
+// Dalam bahasa, c fungsi main adalah tempat dimana kita akan menjalankan programmnya, mirip dengan fungsi <body> di html
+// Int main, pakai Int agar main return integer diakhir program, kalau programmnya lancar, maka harusnya return 0, jika returnnya diatas 0 maka program ada yang error
