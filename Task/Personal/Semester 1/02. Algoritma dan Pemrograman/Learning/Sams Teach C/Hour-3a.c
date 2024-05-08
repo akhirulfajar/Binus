1da45bc85f9c3a32dfd2f2795ef07cd722abbe01
akhirulfajar <akhirulfajar@gmail.com>
2024-05-08 23:09:27 +0700

    Update on hour 3, doing more exercise of using a functions

---
+++ Task/Personal/Semester 1/02. Algoritma dan Pemrograman/Learning/Sams Teach C/Hour-3a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int kalikan_tujuh (int x)
+{
+    int hasil;
+    hasil = x * 17;
+    return hasil;
+}
+
+
+int main()
+{
+    int angka;
+    
+    angka = kalikan_tujuh(37);
+    printf("Hasil dari 37 dikalikan tujuh adalah %d. \n", angka);
+    
+}
