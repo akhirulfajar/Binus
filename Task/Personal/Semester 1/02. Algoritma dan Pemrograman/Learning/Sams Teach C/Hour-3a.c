9382cd7866b79c205d3010e488cb4899e6d89a06
akhirulfajar <akhirulfajar@gmail.com>
2024-05-08 23:31:26 +0700

    Update on hour 3

--- Task/Personal/Semester 1/02. Algoritma dan Pemrograman/Learning/Sams Teach C/Hour-3a.c
+++ Task/Personal/Semester 1/02. Algoritma dan Pemrograman/Learning/Sams Teach C/Hour-3a.c
@@ -13,6 +13,6 @@ int main()
     int angka;
     
     angka = kalikan_tujuh(37);
-    printf("Hasil dari 37 dikalikan tujuh adalah %d. \n", angka);
+    printf("Hasil dari 37 dikalikan  tujuh adalah %d. \n", angka);
     
 }
